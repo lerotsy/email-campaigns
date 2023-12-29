047a225e2147cdaf2f28bc57c7a6bbaaac677053
lerotsy <rotsyrazafy@gmail.com>
2023-12-29 16:50:03 -0500

    Add campaign & template model and migration

---
+++ app/Models/Campaign.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Campaign extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'template_id',
+        'name',
+        'subject',
+        'content',
+        'scheduled_at',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function template()
+    {
+        return $this->belongsTo(Template::class);
+    }
+}
